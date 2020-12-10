18fd03f1583c37aaa16398dfda5dc5c0f3d6442d
kentaroSawada654 <sawada301c@gmail.com>
2020-12-11 01:59:45 +0900

    stateの値をlocalstrageに保存

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState} from "react";
 import {Todo} from './components/todo';
 import {Incomplete} from './components/incomplete';
 import {Complete} from './components/complete';
@@ -9,24 +9,12 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-const APP_KEY = 'sampleApp'
 
 function App() {
 
-  //ローカルストレージ
-  const appState = localStorage.getItem(APP_KEY)
-  const initialState = appState ? JSON.parse(appState) : {
-    events: []
-  }
-
-  const [state, dispatch] = useReducer(reducer, initialState)
-  useEffect(() => {
-    localStorage.setItem(APP_KEY, JSON.stringify(state))
-  }, [todo, incomplete, complete])
-
-  const [todo,setTodo] = useState("");
-  const [incomplete,setIncomplete] = useState([]);
-  const [complete,setComplete] = useState([]);
+  const [todo, setTodo] = useLocalStorage("todo","");//todoって名前でlocalstrageに保存
+  const [incomplete, setIncomplete] = useLocalStorage("incomplete",[]);//保存
+  const [complete, setComplete] = useLocalStorage("complete", []);//保存
 
   const onChangeTodo = (event)=>{
     setTodo(event.target.value)
@@ -70,7 +58,42 @@ function App() {
   }
 
 
+  // useStateの値をlocalstrageに保存
+  function useLocalStorage(key, initialValue) {
+    // State to store our value
+    // Pass initial state function to useState so logic is only executed once
+    const [storedValue, setStoredValue] = useState(() => {
+      try {
+        // Get from local storage by key
+        const item = window.localStorage.getItem(key);
+        // Parse stored json or if none return initialValue
+        return item ? JSON.parse(item) : initialValue;
+      } catch (error) {
+        // If error also return initialValue
+        console.log(error);
+        return initialValue;
+      }
+    });
 
+    // Return a wrapped version of useState's setter function that ...
+    // ... persists the new value to localStorage.
+    const setValue = value => {
+      try {
+        // Allow value to be a function so we have same API as useState
+        const valueToStore =
+          value instanceof Function ? value(storedValue) : value;
+        // Save state
+        setStoredValue(valueToStore);
+        // Save to local storage
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      } catch (error) {
+        // A more advanced implementation would handle the error case
+        console.log(error);
+      }
+    };
+
+    return [storedValue, setValue];
+  }
 
   return (
     <div className="App">
